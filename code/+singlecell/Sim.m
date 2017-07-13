0d757543ec6f8e85f2f5e4821c2b9284ef730e53
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2017-07-13 17:49:40 +0300

    Fix coupling of singlecell in pde as source

--- code/+singlecell/Sim.m
+++ code/+singlecell/Sim.m
@@ -2,22 +2,41 @@
 classdef Sim < handle
 
 properties
-  tstart;
-  y;
+  stateHistory;
 end
 
 methods
   function this= Sim(y0,t0)
-    this.y= y0;
-    if nargin==1, this.tstart= 0;
-    else, this.tstart= t0; end
+    this.stateHistory.y(:,1)= y0;
+    this.stateHistory.t(1)= 0;
+    this.stateHistory.latest= 1;
+    if nargin==2, this.stateHistory.t(1)= t0; end
   end
-  function extraAHL= step(this,AHL,tstop)
-    this.y(6)= AHL;
-    this.y= singlecell.run(this.y,[this.tstart tstop]);
-    this.y= this.y(end,:);
-    this.tstart= this.tstart + tstop;
-    extraAHL= this.y(6)-AHL;
+  function ahlProd= step(this,AHL,tstop)
+    ahlProd= 0;
+    latest= this.rollback(tstop);
+    if latest>0
+      y= this.stateHistory.y(:,latest);
+      tstart= this.stateHistory.t(latest);
+      y= singlecell.run(y,[tstart tstop])';
+      ahlProd= (y(6,end) - AHL) ./ (tstop - tstart);
+      this.stateHistory.y(:,latest+1)= y(:,end);
+      this.stateHistory.y(6,latest+1)= AHL;     % Enforce pde AHL value
+      this.stateHistory.t(latest+1)= tstop;
+      this.stateHistory.latest= latest+1;
+    end
+  end
+end
+
+methods (Access= private)
+  function latest= rollback(this,tstop)
+    latest= 0;
+    for i= this.stateHistory.latest:-1:1
+      if this.stateHistory.t(i) < tstop
+        latest= i;
+        return;
+      end
+    end
   end
 end
 end
