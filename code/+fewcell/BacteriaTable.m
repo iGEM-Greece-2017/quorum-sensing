0eca50e99b87596c6e7b5448056b97bdf6907e36
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2017-07-13 18:02:33 +0300

    Update distributed model

--- code/+fewcell/BacteriaTable.m
+++ code/+fewcell/BacteriaTable.m
@@ -4,35 +4,49 @@ classdef BacteriaTable < handle
 
 properties
   cellModels;   % Vector of singlecell models
+  groupQuery= false;
+  lastAHLProd= 0;
 end
 
 methods
-  function this= BacteriaTable(nBact)
-    y0= [1.6E-7;0;0;0;0;0;0;0;0];
-    t0= 0;
+  function this= BacteriaTable(nBact,y0,t0)
     % TODO: initialize singlecell.Sim correctly (check all objects)
-    cellModels(nBact)= singlecell.Sim(y0,t0);
+    for b= 1:nBact+1, cellModels(b)= singlecell.Sim(y0,t0); end
     this.cellModels= cellModels;
   end
+  
   % This function is meant to be called by the pde solver on all nodes
   % inside a bacterium
   function extraAHL= AHLProd(this,region,state)
+    if any(isnan(state.u)), extraAHL= nan(size(state.u)); return; end
+    if state.time < 1e-6, extraAHL= zeros(size(state.u)); return; end
     n= size(region.x);    % How many points
-    if state.time==0, extraAHL= zeros(n); return; end
-    cell= this.routeCoordCell(region);
-    %region
-    state
-    region.subdomain
-    %extraAHL= this.cellModels(cell).step(state.u,state.time);
-    extraAHL= 1e-6;
+    
+    cellIdx= this.routeCoordCell(region);
+    extraAHL= zeros(size(region.x));
+    extraAHL(cellIdx==0)= 0;
+    for i= unique(cellIdx)
+      if i>0
+        % Pass state.time to singlecell and let it match the given time with the nearest time
+        % asked in the past, then load that state, erase all future times and roll forward
+        % to the given time
+        extraAHL(cellIdx==i)= this.cellModels(i).step(...
+          mean(state.u(cellIdx==i)),state.time) ./ sum(cellIdx==i);
+      end
+    end
+    %fprintf('[BT::AHLProd]: n=%d\t extraAHL=%.2g\t time: %.2g\n', prod(n), max(extraAHL), max(state.time));
   end
 end
 
 methods (Access= private)
   % Maps coordinates to singlecell models and creates new models, if required
-  function cellID= routeCoordCell(this,coord)
-    % TODO
-    cellID= 1;
+  function cellIdx= routeCoordCell(this,region)
+    % Assigns each element to a cell.
+    % Output:
+    % - idx {size(region.x)}: cell to which each element is assigned (0 => no cell)
+    
+    % TODO: Only for 1 bact!
+    cellIdx= abs(round(region.subdomain*1e300*1e23)-1);
   end
 end
 end
--- code/+fewcell/distributedModel.m
+++ code/+fewcell/distributedModel.m
@@ -1,74 +1,95 @@
+%% Parameters
+y0= [1.6E-7;0;0;0;0;0;0;0;0];   % singlecell initial conditions
+tstop= 60*30;  % sec
+
+bactSize= [2;1]*1e-6;
+bactCenters= [0,0];
+domainLim= 5e-5;
+zoominFactor= 2;    % When visualizing, focus on a small part of the domain
+
+interpResolution= 60;
+timeSubsampling= tstop/10;
+dynamicScaling= true;
+
+%% Time discretization
+ntime= tstop*10+1;
+tlist= linspace(0,tstop,ntime);
 %% Geometry
-bactSize= [1;2]*1e-6;
-bact= [4;0;0;bactSize;0];  % ellipse
-domainLim= 2e-5;
-%domain= [3;4;[-1;-1;1;1;-1;1;1;-1]*domainLim]; % square
-d=(0:5)'*pi/3;
-domain= [2;6; [cos(d); sin(d)]*domainLim];
-bact= [bact;zeros(length(domain)-length(bact),1)];
-geomNames= char('domain','bact')';
-dl= decsg([domain,bact], 'domain+bact', geomNames);
+geometryFun= @(varargin)fewcell.bactDomainGeom(bactCenters,bactSize,domainLim,varargin);
+domainArea= (2*domainLim + domainLim) * sin(pi/3)*domainLim;  % hexagon:= 2*trapezoids
 %% PDE
 numPde= 1;
 model= createpde(numPde);
-geometryFromEdges(model,dl);
+geometryFromEdges(model,geometryFun);
+%{
+% Plot pde domain
+figure(1);
+pdegplot(model);%,'EdgeLabels','on','FaceLabels','on');
+drawnow;
+axis equal;
+%}
 
 %% Boundaries
 applyBoundaryCondition(model,'neumann','Edge',1:6,'g',0,'q',0);
-applyBoundaryCondition(model,'neumann','Edge',7:10,'g',0,'q',100);
+applyBoundaryCondition(model,'neumann','Edge',7:8,'g',0,'q',100);
 %% PDE coeffs
 %prodFun= @(~,st)(exp(-st.u*1e10)-0.85)*20e-6;
-bacteriaTable= fewcell.BacteriaTable(1);
+bacteriaTable= fewcell.BacteriaTable(1,y0,tlist(1));
 % d: d*u', c: -div(c*grad(u)), f: source
 specifyCoefficients(model, 'Face',1, 'm',0, 'd',1, 'c',1e-9, 'a',0, 'f',0);
 specifyCoefficients(model, 'Face',2, 'm',0, 'd',1, 'c',1e-9, 'a',0, 'f',@(r,s)bacteriaTable.AHLProd(r,s));
 %% Initial conditions
 setInitialConditions(model,0);
-%{
-% Plot pde domain
-figure(1);
-pdegplot(model,'EdgeLabels','on','FaceLabels','on');
-drawnow;
-axis equal;
-%}
 %% Mesh
-mesh= generateMesh(model,'MesherVersion','R2013a','GeometricOrder','linear', ...
-  'Hgrad',1.15,'Hmax',2*domainLim /1);
-%{
+mesh= generateMesh(model,'MesherVersion','preR2013a','GeometricOrder','linear', ...
+  'Hgrad',1.4,'Hmax',2*domainLim /12);
+totalMeshNodes= size(model.Mesh.Nodes,2)
+%
 % Plot mesh
 figure(2); clf;
 pdeplot(model);
 drawnow;
 axis equal;
 %}
-%% Time discretization
-nframes= 200;
-tlist= logspace(-3.5,-1.7,nframes);
 
 %% Solve
-fprintf('Solving...\n');
+fprintf('--> Solving...\n');
 tic;
-model.SolverOptions.ReportStatistics= 'off';
+model.SolverOptions.AbsoluteTolerance= 1e-15;
+model.SolverOptions.RelativeTolerance= 1e-6;
+model.SolverOptions.ResidualTolerance= 1e-7;
+model.SolverOptions.MaxIterations= 50;
+model.SolverOptions.MinStep= min(bactSize)/10;
+model.SolverOptions.ReportStatistics= 'on';
 result= solvepde(model,tlist);
-%umin= abs(2*min(result.NodalSolution(:)));
 toc;
 
 %% Plot solution
 % Prepare solution interpolation
-fprintf('Interpolating solution...\n');
+fprintf('--> Interpolating solution...\n');
 tic;
-[AHLDistrib,x,y]= fewcell.util.interpolateAHL(result,domainLim*0.8,100);
-totalAHL= fewcell.util.integrateAHL(result,domainLim*0.8);
+[AHLDistrib,x,y,interpTimes,totalAHL]= ...
+  fewcell.util.interpolateIntegrateAHL(model,result,domainLim,zoominFactor, ...
+                                       interpResolution, timeSubsampling);
 toc;
 % Plot
-fprintf('Plotting solution...\n');
+fprintf('--> [paused] Press key to plot the solution\n');
+pause;
+fprintf('--> Plotting solution...\n');
 global distribAHL_interp_graphics;
 distribAHL_interp_graphics= []; distribAHL_interp_graphics.first= true;
-for t=30:nframes
+nframes= length(interpTimes);
+for t=1:nframes
   tic;
-  fewcell.viz.distribAHL_interp(AHLDistrib,x,y,totalAHL,result.SolutionTimes,...
-    domainLim,false,3,t);
+  fewcell.viz.distribAHL_interp(AHLDistrib,x,y,totalAHL,interpTimes,...
+    domainLim/zoominFactor,dynamicScaling,3,t);
   % Control framerate
-  pauseTime= 1/35 - toc;
+  pauseTime= 1/62 - toc;
   if pauseTime>1e-4, pause(pauseTime); end
 end
+
+% Printed results
+% ref: http://book.bionumbers.org/what-is-the-concentration-of-bacterial-cells-in-a-saturated-culture/
+bacterialDensity= 1/(domainArea*1e4); % [bacteria/cm^2]
+fprintf('Bacterial density: %.2g bacteria/cm^2\t %.2g bacteria/mL (stacked tightly)\n', bacterialDensity, bacterialDensity*1e-2/(2*min(bactSize)));
+fprintf('Final total [AHL] at t=%.1fsec: %.2g\n\n', interpTimes(end), totalAHL(end));
