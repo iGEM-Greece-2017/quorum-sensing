d0863cf87b385e3555050de72194ae41179d908d
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2017-07-18 00:44:03 +0300

    Repackage old fewcell to accommodate new

--- code/+fewcell/+topview/distribModel.m
+++ code/+fewcell/+topview/distribModel.m
@@ -1,21 +1,21 @@
 %% Parameters
-y0= [1.6E-7;0;0;0;0;0;0;0;0];   % singlecell initial conditions
-tstop= 60*100;  % sec
+y0= [1.6E-7;0;0;0;0;0;0;0;0]*1e-9;   % singlecell initial conditions
+tstop= 60*5000;  % sec
 
-bactSize= [2;1]*1e-6;
-bactCenters= [0,0];
-domainLim= 50e-5;
+bactSize= [1.5;1]*1e-6;
+bactCenters= [0,0]*1e-6;
+domainLim= 1e-4;
 zoominFactor= 1;    % When visualizing, focus on a small part of the domain
 
-interpResolution= 50;
-timeSubsampling= tstop/8;
+interpResolution= 80;
+timeSubsampling= tstop/80;
 dynamicScaling= true;
 
 %% Time discretization
-ntime= tstop*10+1;
-tlist= linspace(0,tstop,ntime);
+ntime= tstop+1;
+tlist= linspace(30,tstop,ntime);
 %% Geometry
-geometryFun= @(varargin)fewcell.bactDomainGeom(bactCenters,bactSize,domainLim,varargin);
+geometryFun= @(varargin)fewcell.topview.bactDomainGeom(bactCenters,bactSize,domainLim,varargin);
 domainArea= (2*domainLim + domainLim) * sin(pi/3)*domainLim;  % hexagon:= 2*trapezoids
 %% PDE
 numPde= 1;
@@ -30,25 +30,31 @@ axis equal;
 %}
 
 %% Boundaries
+nBact= size(bactCenters,1);
 applyBoundaryCondition(model,'neumann','Edge',1:6,'g',0,'q',0);
-applyBoundaryCondition(model,'neumann','Edge',7:8,'g',0,'q',100);
+for b= 1:nBact
+  applyBoundaryCondition(model,'neumann','Edge',(1:2)+6+(b-1)*2,'g',0,'q',100);
+end
 %% PDE coeffs
 %prodFun= @(~,st)(exp(-st.u*1e10)-0.85)*20e-6;
-bacteriaTable= fewcell.BacteriaTable(1,y0,tlist(1));
+%bacteriaTable= fewcell.topview.BacteriaTable(1,y0,tlist(1));
 % d: d*u', c: -div(c*grad(u)), f: source
-specifyCoefficients(model, 'Face',1, 'm',0, 'd',1, 'c',1e-9, 'a',0, 'f',0);
-specifyCoefficients(model, 'Face',2, 'm',0, 'd',1, 'c',1e-7, 'a',0, 'f',@(r,s)bacteriaTable.AHLProd(r,s));
+specifyCoefficients(model, 'Face',1, 'm',0, 'd',1, 'c',1e-9, 'a',1/3, 'f',0);
+for b= 1:nBact
+  % Dummy AHL production. Helps identify the nodes of the bacterium
+  specifyCoefficients(model, 'Face',b+1, 'm',0, 'd',1, 'c',1e-7, 'a',0, 'f',@(r,s)b*(s.u+1));
+end
 %% Initial conditions
 setInitialConditions(model,0);
 %% Mesh
 mesh= generateMesh(model,'MesherVersion','R2013a', 'Jiggle','minimum', 'JiggleIter',25,...
-  'GeometricOrder','linear', 'Hgrad',1.8,'Hmax',domainLim /8);
+  'GeometricOrder','linear', 'Hgrad',1.9,'Hmax',domainLim /6);
 totalMeshNodes= size(model.Mesh.Nodes,2);
 fprintf('Total mesh nodes: %d\n', totalMeshNodes);
-%
+%{
 % Plot mesh
 figure(2); clf;
-pdeplot(model);
+pdeplot(model);%,'NodeLabels','on');
 drawnow;
 axis equal;
 %}
@@ -56,9 +62,9 @@ axis equal;
 %% Solve
 fprintf('--> Solving...\n');
 tic;
-model.SolverOptions.AbsoluteTolerance= 1e-15;   % critical: 2e-16
-model.SolverOptions.RelativeTolerance= 1e-8;
-model.SolverOptions.ResidualTolerance= 1e-9;
+model.SolverOptions.AbsoluteTolerance= 1e-25;
+model.SolverOptions.RelativeTolerance= 1e-12;
+model.SolverOptions.ResidualTolerance= 1e-12;
 model.SolverOptions.MaxIterations= 50;
 model.SolverOptions.MinStep= min(bactSize)/10;
 model.SolverOptions.ReportStatistics= 'on';
@@ -80,7 +86,7 @@ fprintf('--> Plotting solution...\n');
 global distribAHL_interp_graphics;
 distribAHL_interp_graphics= []; distribAHL_interp_graphics.first= true;
 nframes= length(interpTimes);
-for t=1:nframes
+for t=10:nframes
   tic;
   fewcell.viz.distribAHL_interp(AHLDistrib,x,y,totalAHL,interpTimes,...
     domainLim/zoominFactor,dynamicScaling,3,t);
@@ -93,4 +99,4 @@ end
 % ref: http://book.bionumbers.org/what-is-the-concentration-of-bacterial-cells-in-a-saturated-culture/
 bacterialDensity= 1/(domainArea*1e4); % [bacteria/cm^2]
 fprintf('Bacterial density: %.2g bacteria/cm^2\t %.2g bacteria/mL (stacked tightly)\n', bacterialDensity, bacterialDensity*1e-2/(2*min(bactSize)));
-fprintf('Final total [AHL] at t=%.1fsec: %.2g\n\n', interpTimes(end), totalAHL(end));
+fprintf('Final total [AHL] at t=%.1fsec: %.3g\n\n', interpTimes(end), totalAHL(end));
