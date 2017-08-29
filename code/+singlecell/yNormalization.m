ca1a687994ce2b2299920b14c31dac986fec84b9
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2017-08-29 20:30:02 +0300

    Rm old code

--- code/+singlecell/yNormalization.m
+++
@@ -1,6 +0,0 @@
-function ycoeff= yNormalization(selectCoeffs)
-% Transform the system's dependent variables to keep their values closer to 1
-
-%ycoeff= [1e1,1,1,1e-3,1e-2,1e-1,1e-2,1e-4,1,1e-1];
-ycoeff= [1 1 1 1 1 1 1 1 1 1];
-ycoeff= ycoeff(selectCoeffs);
