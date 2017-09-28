545dda2b23a3d3b71933bdb96f645dee88ac15b1
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2017-09-28 23:06:00 +0300

    Store results to file, display later

---
+++ code/+fewcell/displayPrecalcResult.m
@@ -0,0 +1,14 @@
+% Display precalculated result
+load('data/tmpresults.mat');
+
+%% Plot solution
+global enableGraphics; enableGraphics= true;
+% Prepare solution interpolation
+fprintf('--> Interpolating solution...\n');
+[AHLDistrib,x,y,interp_t,totalAHL]= ...
+  fewcell.util.interpolateIntegrateAHL(model,result,params);
+% Plot
+%if params.viz.figID(1), fprintf('--> [paused] Press key to plot the solution\n'); pause; end
+fprintf('--> Plotting solution...\n');
+fewcell.output(model,result,tlist,...
+               AHLDistrib,x,y,interp_t,totalAHL,params.viz);
